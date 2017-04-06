9649419702e132bee2fd07d465ac287acb886efe
varis darasirikul <varis.dara@gmail.com>
2017-04-07 00:47:30 +0700

    Update all test

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -3,4 +3,20 @@ import CommentList from '../../src/components/comment_list'
 
 describe('CommentList', () => {
 
+    let component
+
+    beforeEach(() => {
+        const props = { comments: ['New comment', 'Other New Comment'] }
+        component = renderComponent(CommentList, null, props)
+    })
+
+    it('show an LI for each comment', () => {
+        expect(component.find('li').length).to.equal(2)
+    })
+
+    it('show each comment that is provided', () => {
+        expect(component).to.contain('New Comment')
+        expect(component).to.contain('Other New Comment')
+    })
+
 })

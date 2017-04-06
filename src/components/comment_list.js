990b8c13dc92b96e8d3b78164837b8576454d2ca
manjarb <manjarb4@gmail.com>
2017-04-06 12:27:15 +0700

    prepare comment list component

---
+++ src/components/comment_list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const CommentList = (props) => {
+
+    return (
+        <ul className="comment-list">
+
+        </ul>
+    )
+}
+
+export default CommentList

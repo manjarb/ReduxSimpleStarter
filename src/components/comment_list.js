72b544dae735977281fce2b72ecf374b8f682826
manjarb <manjarb4@gmail.com>
2017-04-15 23:18:26 +0700

    tt

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
 const CommentList = (props) => {
+  const list = props.comments.map(comment => <li key={comment}>{comment}</li>);
 
-    return (
-        <ul className="comment-list">
+  return (
+    <ul className="comment-list">{list}</ul>
+  );
+};
 
-        </ul>
-    )
+function mapStateToProps(state) {
+  return { comments: state.comments };
 }
 
-export default CommentList
+export default connect(mapStateToProps)(CommentList);
